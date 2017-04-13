16b4b9ec86feabd5f3594f6548bf85a04d4b5240
Elejimenezp <eleonora_791@hotmail.com>
2017-04-13 11:20:13 -0500

    commit 2 ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Eleonora Jimenez
+## 4/11/2017
+## programmingAssignment2: calculating and storing the inverse of a matric
 
-## Write a short comment describing this function
+## function used to cache de inverse when it's calculated by solveCache and return it when it called by the same function
+##test:
+## my_matrix <- makeCacheMatrix(matrix(c(3,6,2,4), 2, 2))
+## my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+## my_matrix <- makeCacheMatrix(matrix(1:6, 3, 2))
 
 makeCacheMatrix <- function(x = matrix()) {
+	 i <- NULL 
+	 set <- function(y) { 
+                x <<- y
+                i <<- NULL
+	 }
+        get <- function() x  
+        setinverse <- function(solve) i <<- solve 
+        getinverse <- function() i 
+        list(set = set, get = get,setinverse = setinverse, getinverse = getinverse) 
 
 }
 
-
-## Write a short comment describing this function
-
+#Calculates the inverse of the matrix if it's not i the cache or returns it using makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	  i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        tryCatch(i <- solve(data, ...),
+			error = function(msg){
+			message("there has been the following error:")
+			message(msg)
+			return(NULL)
+			},
+			warning = function(msg){
+			message("there has been the following warning:")
+			message(msg)		
+			x$setinverse(i)
+        		return(i)
+			},
+        		finally ={
+			x$setinverse(i)
+			return(i)})
+
 }
